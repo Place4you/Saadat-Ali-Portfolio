d543d8a51aa90d2fed8dddc278f9ef4c69719a1d
Place4you <66410030+Place4you@users.noreply.github.com>
2023-11-21 18:56:46 +0500

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./build/**/*.{html,js}",
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  safelist: ['outline-none'],
   theme: {
     extend: {
       backgroundImage: {
