67c9504012cc1f36dec06dd8819c4260e42b7fcf
Place4you <66410030+Place4you@users.noreply.github.com>
2023-11-23 08:05:03 +0500

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   important: true,
+  mode: 'jit',
   content: ["./app/**/*.{js,ts,jsx,tsx}", 
   "./public/**/*.{js,ts,jsx,tsx}", 
   "./components/**/*.{js,ts,jsx,tsx}", ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+     extend: {
+       backgroundImage: {
+         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+         'gradient-conic':
+           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+       },
+     },
   },
   plugins: [],
-}
+ }
