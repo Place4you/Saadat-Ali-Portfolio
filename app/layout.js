673951875149e02e62933276e93c0062f86fc1b8
Place4you <66410030+Place4you@users.noreply.github.com>
2023-11-23 11:25:44 +0500

    head

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import { Inter } from 'next/font/google'
-
+import Head from 'next/head';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,7 +12,8 @@ export default function RootLayout({ children }) {
   return (
     <html className='bg-[#121212] px-6 mx-6' lang="en">
       <head>
-      <link rel="stylesheet" href="../app/globals.css" />
+      <Head> 
+      <link rel="stylesheet" href="../app/globals.css" /></Head>
 
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;700&display=swap" />
       </head>
