a7b02493da99db9375456022a85fad40272e8039
Place4you <66410030+Place4you@users.noreply.github.com>
2023-11-23 11:43:13 +0500

    purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", 
   "./public/**/*.{js,ts,jsx,tsx}", 
   "./components/**/*.{js,ts,jsx,tsx}", ],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  
   theme: {
      extend: {
        backgroundImage: {
