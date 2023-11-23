9fc1b3cbfa5f8077a8696a5b11f22c06d1029148
Place4you <66410030+Place4you@users.noreply.github.com>
2023-11-23 11:32:53 +0500

    changes

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 
 import { Inter } from 'next/font/google'
-import Head from 'next/head';
+import '../app/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,9 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html className='bg-[#121212] px-6 mx-6' lang="en">
       <head>
-      <Head> 
-      <link rel="stylesheet" href="../app/globals.css" /></Head>
-
+      <link rel="stylesheet" href="./globals.css" />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;700&display=swap" />
       </head>
       <body suppressHydrationWarning className={inter.className}>{children}</body>
