e3df1faf5e1e56a069c53dff4ecce62a81d5ceb5
Place4you <66410030+Place4you@users.noreply.github.com>
2023-12-04 14:49:49 +0500

    testing new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,19 @@
 module.exports = {
   important: true,
   mode: 'jit',
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}",
-  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-  "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-  // Or if using `src` directory:
-  "./src/**/*.{js,ts,jsx,tsx,mdx}", ],
-
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-     extend: {
-       backgroundImage: {
-         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-         'gradient-conic':
-           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-       },
-     },
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
   },
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
-  }
- }
+};
