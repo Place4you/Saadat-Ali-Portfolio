86122d1672b6b61216de9996fa97d4455ff3d78e
Obiora Igboanusi <cwertlinks@gmail.com>
2023-12-27 01:49:50 +0100

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,13 +22,5 @@ module.exports = {
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-  },
-    // purge: ["./app/**/*.js", "./components/**/*.js"],
-    purge:[
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    ]
+  }, 
 };
